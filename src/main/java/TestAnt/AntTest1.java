4849a4bba7926dd5d28db473dca44a3daf081948
KTE-521 <KTE-521@DESKTOP-RHO1BQ5>
2021-09-29 15:45:49 +0900

    시험

---
+++ src/main/java/TestAnt/AntTest1.java
@@ -0,0 +1,7 @@
+package TestAnt;
+
+public class AntTest1 {
+	public static void main(String[] args) {
+		System.out.println("이 프로그램은 AntTest1.Java입니다");
+	}
+}
---
+++ src/main/java/TestAnt/AntTest2.java
@@ -0,0 +1,7 @@
+package TestAnt;
+
+public class AntTest2 {
+	public static void main(String[] args) {
+		System.out.println("이 프로그램은 AntTest2.Java입니다");
+	}
+}
